ce6ae7fad8472c03aa5d7cf85982f5a687e598aa
María Luciana Pérez Barrionuevo <lucianapbarrionuevo@hotmail.com>
2024-11-25 23:31:48 -0300

    creacion de HomeScreen solo a modo de visualizacion

---
+++ src/views/HomeScreen.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../css/HomeScreen.css"
+const HomeScreen = () => {
+  return (
+    <div>
+        <div className="home-container d-flex flex-column justify-content-center align-items-center text-center">
+        <h1>HomeScreen</h1>
+      </div>
+    </div>
+  )
+}
+
+export default HomeScreen
