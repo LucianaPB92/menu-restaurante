42cbfdc63324d1cb5aa49f91b513922de91816d9
María Luciana Pérez Barrionuevo <lucianapbarrionuevo@hotmail.com>
2024-11-29 12:23:14 -0300

    actualizacion de url

--- src/helpers/fetchApi.js
+++ src/helpers/fetchApi.js
@@ -1,20 +1,18 @@
-const url = "https://webserver-mfl9.onrender.com/api/"; 
-
-// const url ="https://webserver-103i.onrender.com/api/"
+const url = "https://webserver-mfl9.onrender.com/api/";
+// const url = "https://proyecto-menu-de-restaurante.onrender.com/api/"
 
 const auth = async (email, password) => {
   const resp = await fetch(url + "auth/login", {
-    
-    method: "POST", 
-    body: JSON.stringify({ email, password }), 
+    method: "POST",
+    body: JSON.stringify({ email, password }),
     headers: {
-      "Content-type": "application/json; charset=UTF-8", 
+      "Content-type": "application/json; charset=UTF-8",
     },
   });
 
   const data = await resp.json();
 
-  return data; 
+  return data;
 };
 
-export { auth }; 
+export { auth };
