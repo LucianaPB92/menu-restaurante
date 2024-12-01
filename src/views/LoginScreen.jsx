33f529f59983df28705a85341c0f839bc268e337
María Luciana Pérez Barrionuevo <lucianapbarrionuevo@hotmail.com>
2024-11-30 23:14:33 -0300

    adicion de loader en login

--- src/views/LoginScreen.jsx
+++ src/views/LoginScreen.jsx
@@ -4,9 +4,11 @@ import { useNavigate } from "react-router-dom";
 import { auth } from "../helpers/fetchApi";
 import RegisterModal from "../components/ModalRegisterApp";
 import "../css/LoginScreen.css";
+
 const LoginScreen = () => {
-  const navigate = useNavigate(); // Instancia de useNavigate
+  const navigate = useNavigate();
   const [message, setMessage] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
   const {
     register: loginRegister,
     handleSubmit: handleLoginSubmit,
@@ -15,68 +17,83 @@ const LoginScreen = () => {
 
   const logIn = (data, e) => {
     e.preventDefault();
-
+    setIsLoading(true);
     const { correo, pass } = data;
-    auth(correo, pass).then((response) => {
-      if (response?.token) {
-        localStorage.setItem("token", JSON.stringify(response.token));
-        localStorage.setItem("uid", JSON.stringify(response.uid));
-        navigate("/");
-      } else {
-        setMessage(response);
-      }
-    });
+    auth(correo, pass)
+      .then((response) => {
+        console.log("Respuesta de auth:", response);
+        if (response?.token) {
+          localStorage.setItem("token", JSON.stringify(response.token));
+          localStorage.setItem("uid", JSON.stringify(response.uid));
+          navigate("/");
+        } else {
+          setMessage(response);
+        }
+      })
+      .catch((error) => {
+        console.error("Error al autenticar:", error);
+        setMessage({ msg: "Error en el servidor, intenta más tarde" });
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   };
 
   return (
     <div>
       <div className="login-container d-flex flex-column justify-content-center align-items-center">
         <h2 className="mb-4">Iniciar Sesión</h2>
-        <form className="login-form" onSubmit={handleLoginSubmit(logIn)}>
-          <div className="mb-3">
-            <label htmlFor="email" className="form-label">
-              Correo Electrónico
-            </label>
-            <input
-              type="email"
-              className={
-                loginErrors.correo?.type === "required"
-                  ? "form-control bg-danger"
-                  : "form-control"
-              }
-              id="email"
-              placeholder="Ingresa tu correo electrónico"
-              {...loginRegister("correo", { required: true })}
-            />
-            {loginErrors.correo?.type === "required" && (
-              <p className="text-danger">Correo obligatorio</p>
-            )}
+        {isLoading ? (
+          <div className="spinner-border text-primary" role="status">
+            <span className="visually-hidden">Cargando...</span>
           </div>
-          <div className="mb-3">
-            <label htmlFor="password" className="form-label">
-              Contraseña
-            </label>
-            <input
-              type="password"
-              className="form-control"
-              id="password"
-              placeholder="Ingresa tu contraseña"
-              {...loginRegister("pass", { required: true })}
-            />
-            {loginErrors.pass?.type === "required" && (
-              <p className="text-danger">Contraseña obligatoria</p>
-            )}
-          </div>
-          <button type="submit" className="btn btn-primary">
-            Iniciar Sesión
-          </button>
-        </form>
+        ) : (
+          <form className="login-form" onSubmit={handleLoginSubmit(logIn)}>
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">
+                Correo Electrónico
+              </label>
+              <input
+                type="email"
+                className={
+                  loginErrors.correo?.type === "required"
+                    ? "form-control bg-danger"
+                    : "form-control"
+                }
+                id="email"
+                placeholder="Ingresa tu correo electrónico"
+                {...loginRegister("correo", { required: true })}
+              />
+              {loginErrors.correo?.type === "required" && (
+                <p className="text-danger">Correo obligatorio</p>
+              )}
+            </div>
+            <div className="mb-3">
+              <label htmlFor="password" className="form-label">
+                Contraseña
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                placeholder="Ingresa tu contraseña"
+                {...loginRegister("pass", { required: true })}
+              />
+              {loginErrors.pass?.type === "required" && (
+                <p className="text-danger">Contraseña obligatoria</p>
+              )}
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Iniciar Sesión
+            </button>
+          </form>
+        )}
         {message && (
           <div className="alert alert-danger mx-2" role="alert">
             {message.msg}
           </div>
         )}
-         <button
+        <button
           type="button"
           className="btn btn-link mt-3"
           data-bs-toggle="modal"
@@ -85,8 +102,8 @@ const LoginScreen = () => {
           ¿No tienes una cuenta? Regístrate aquí
         </button>
       </div>
-       {/* Componente RegisterModal */}
-       <RegisterModal id="userRegisterModal" defaultRole="USER_ROLE" />
+
+      <RegisterModal id="userRegisterModal" defaultRole="USER_ROLE" />
     </div>
   );
 };
