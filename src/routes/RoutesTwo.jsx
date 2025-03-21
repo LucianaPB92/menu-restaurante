65c9b0489d3b6be6148a95743b29028a1e5c21bc
María Luciana Pérez Barrionuevo <lucianapbarrionuevo@hotmail.com>
2025-03-21 18:35:05 -0300

    modificacion de rutas secundarias

--- src/routes/RoutesTwo.jsx
+++ src/routes/RoutesTwo.jsx
@@ -1,24 +1,41 @@
 import React from 'react'
 import { Route, Routes } from "react-router-dom";
-import ProtectedRoutes from './ProtectedRoutes';
+import ProtectedRoutes from "./ProtectedRoutes";
 import HomeScreen from '../views/HomeScreen'
-import NavBarApp from '../components/NavbarApp';
-import AdminScreen from '../views/AdminScreen';
+import AdminUserScreen from '../views/AdminUserScreen.jsx';
+import AdminProductoScreen from '../views/AdminProductoScreen.jsx';
+import AdminPedidoScreen from '../views/AdminPedidoScreen.jsx';
 const RoutesTwo = () => {
   return (
     <div>
-        <NavBarApp />
         <Routes>
           <Route path="/" element={<HomeScreen/>}/>
           <Route
-          path="/admin"
+          path="/admin/usuarios"
           element={
-            <ProtectedRoutes>
-              <AdminScreen />
+            <ProtectedRoutes rolesPermitidos={["ADMIN_ROLE"]}>
+              <AdminUserScreen />
+            </ProtectedRoutes>
+          }
+        />
+        <Route
+          path="/admin/productos"
+          element={
+            <ProtectedRoutes rolesPermitidos={["ADMIN_ROLE", "MOD_ROLE", "VENTAS_ROLE"]}>
+              <AdminProductoScreen />
+            </ProtectedRoutes>
+          }
+        />
+         <Route
+          path="/admin/pedidos"
+          element={
+            <ProtectedRoutes rolesPermitidos={["ADMIN_ROLE", "VENTAS_ROLE", "MOD_ROLE"]}>
+              <AdminPedidoScreen />
             </ProtectedRoutes>
           }
         />
         </Routes>
+        
     </div>
   )
 }
