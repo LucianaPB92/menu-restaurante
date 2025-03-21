71c6133387690ce034d81ba91cace881e31bfd1a
María Luciana Pérez Barrionuevo <lucianapbarrionuevo@hotmail.com>
2025-03-21 18:34:47 -0300

    adicion de funciones para rutas protegidas

--- src/routes/ProtectedRoutes.jsx
+++ src/routes/ProtectedRoutes.jsx
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
-
-const ProtectedRoutes = ({ children }) => {
+import { getUsuario } from "../helpers/apiUsuarios";
+const ProtectedRoutes = ({ children, rolesPermitidos }) => {
+  const [isAuthorized, setIsAuthorized] = useState(null);
   const token = JSON.parse(localStorage.getItem("token")) || null;
-  if (token) {
-    return children;
-  } else {
-    return <Navigate to="/login" />;
-  }
+  const uid = JSON.parse(localStorage.getItem("uid")) || null;
+  useEffect(() => {
+    if (token && uid) {
+      getUsuario(uid)
+        .then((response) => {
+          if (
+            response?.usuario &&
+            rolesPermitidos.includes(response.usuario.rol)
+          ) {
+            setIsAuthorized(true);
+          } else {
+            setIsAuthorized(false);
+          }
+        })
+        .catch((error) => {
+          console.error("Error al obtener usuario:", error);
+        });
+    } else {
+      setIsAuthorized(false);
+    }
+  }, [token, uid, rolesPermitidos]);
+
+  if (isAuthorized === null) return null;
+  return isAuthorized ? children : <Navigate to="/" />;
+ 
 };
 
 export default ProtectedRoutes;
