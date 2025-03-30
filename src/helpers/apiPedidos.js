d5d4a2b8c1e5743073ee38925ef5c2add8744b4b
Juan Loretto <juanloretto29@gmail.com>
2025-03-30 16:43:52 -0300

    correccion en getPedidoId

--- src/helpers/apiPedidos.js
+++ src/helpers/apiPedidos.js
@@ -1,61 +1,98 @@
 const url = "https://webserver-qcd7.onrender.com/api/pedidos";
+const getToken = () => JSON.parse(localStorage.getItem("token"));
 const getPedidos = async () => {
-  const resp = await fetch(url, {
-    method: "GET",
-    headers: {
-      "Content-type": "application/json; charset=UTF-8",
-      "x-token": JSON.parse(localStorage.getItem("token")),
-    },
-  });
-  const data = await resp.json();
-  console.log(data);
-  
-  return data;
-  
+  try {
+    const resp = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+        "x-token": getToken()
+      },
+    });
+
+    if (!resp.ok) throw new Error("Error al obtener pedidos");
+
+    const data = await resp.json();
+    return data;
+  } catch (error) {
+    console.error("Error en getPedidos:", error);
+    return null;
+  }
 };
+
 const getPedidoId = async (id) => {
-  const resp = await fetch(url + "/" + id, {
-    method: "GET",
-    headers: {
-      "Content-type": "application/json; charset=UTF-8",
-      "x-token": JSON.parse(localStorage.getItem("token")),
-    },
-  });
-  const data = await resp.json();
-  return data;
+  try {
+    const resp = await fetch(url + "/" + id, {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+        "x-token": getToken(),
+      },
+    });
+
+    if (!resp.ok) throw new Error("No se encontró el pedido");
+
+    return await resp.json();
+  } catch (error) {
+    console.error("Error en getPedidoId:", error);
+    return null;
+  }
 };
+
 const crearPedido = async (datos) => {
-  const resp = await fetch(url, {
-    method: "POST",
-    body: JSON.stringify(datos),
-    headers: {
-      "Content-type": "application/json; charset=UTF-8",
-      "x-token": JSON.parse(localStorage.getItem("token")),
-    },
-  });
-  const data = await resp.json();
-  return data;
+  try {
+    const resp = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(datos),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+        "x-token": getToken(),
+      },
+    });
+    if (!resp.ok) throw new Error("Error al crear el pedido");
+    const data = await resp.json();
+    return data;
+  } catch (error) {
+    console.error("Error al crear el pedido", error);
+    return null;
+  }
 };
 const actualizarPedido = async (id, datos) => {
-  const resp = await fetch(url + "/" + id, {
-    method: "PUT",
-    body: JSON.stringify(datos),
-    headers: {
-      "Content-type": "application/json; charset=UTF-8",
-      "x-token": JSON.parse(localStorage.getItem("token")),
-    },
-  });
-  const data = await resp.json();
-  return data;
+  try {
+    const resp = await fetch(url + "/" + id, {
+      method: "PUT",
+      body: JSON.stringify(datos),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+        "x-token": getToken(),
+      },
+    });
+    if (!resp.ok) throw new Error("Error al actualizar el pedido");
+    const data = await resp.json();
+    return data;
+  } catch (error) {
+    console.error("error al actualizar el pedido", error);
+    return null;
+  }
 };
+
 const eliminarPedido = async (id) => {
-  const resp = await fetch(url + "/" + id, {
-    method: "DELETE",
-    headers: {
-      "Content-type": "application/json; charset=UTF-8",
-      "x-token": JSON.parse(localStorage.getItem("token")),
-    },
-  });
+  try {
+    const resp = await fetch(url + "/" + id, {
+      method: "DELETE",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+        "x-token": getToken(),
+      },
+    });
+
+    if (!resp.ok) throw new Error("No se pudo eliminar el pedido");
+
+    return { success: true };
+  } catch (error) {
+    console.error("Error en eliminarPedido:", error);
+    return { success: false };
+  }
 };
 export {
   getPedidoId,
